34214927ad533e6a8cd24d7073f0205f2d7dcac5
Sachin Tom <sachin.tom.dev@gmail.com>
2025-04-15 13:48:52 +0530

    feat: adding text,view and image components

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,30 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+import Logo from "../assets/img/logo_light.png";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Image source={Logo} style={styles.img} />
+      <Text style={[styles.title, { color: "red" }]}>Big Title</Text>
+      <Text>Reading list app</Text>
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  img: {
+    marginVertical: 20,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 36,
+    margin: 10,
+  },
+});
